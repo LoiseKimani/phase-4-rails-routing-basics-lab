26821c63b6ebe26a676009f5d35c9e1f0a81db44
Loise <loise.kimani@student.moringaschool.com>
2023-03-19 11:40:46 +0300

    Done

--- app/controllers/StudentsController.rb
+++ app/controllers/StudentsController.rb
@@ -8,5 +8,10 @@ class StudentsController < ApplicationController
       students = Student.order(grade: :desc)
       render json: students
     end
+
+    def highest_grade
+      student = Student.order(grade: :desc).first
+      render json: student
+    end
 end
   
